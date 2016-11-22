881129e2b7e48bcd9de8fa52df42b42b90117b95
CesarOmarRoman <MacCesarR@MacBookP-de-Cesar.local>
2016-11-21 20:03:44 -0600

    SolicitarLibro
    SolicitarLibro

---
+++ SolicitarLibro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SolicitarLibro
+//
+//  Created by César Omar Román Domínguez on 21/11/16.
+//  Copyright © 2016 César Omar Román Domínguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
